8f8c15d966b878b1c4ec564467587b8879ada0f3
Jeongminku <jmk00140@gmail.com>
2023-02-15 15:04:33 +0900

    init

---
+++ src/main/java/com/Tingle/G4hosp/G4hospApplication.java
@@ -0,0 +1,13 @@
+package com.Tingle.G4hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G4hospApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G4hospApplication.class, args);
+	}
+
+}
