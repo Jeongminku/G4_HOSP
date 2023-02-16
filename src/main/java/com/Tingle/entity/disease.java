b8cc0c2663027c3cb168b5824a807c1d4e9b96f2
EzenNene <kkhj0822@gmail.com>
2023-02-16 18:40:45 +0900

    hj

---
+++ src/main/java/com/Tingle/entity/disease.java
@@ -0,0 +1,34 @@
+package com.Tingle.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="disease") 
+@Getter
+@Setter
+@ToString
+public class disease {
+
+	@Id
+	@Column(name = "disease_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long diseaseId;
+	
+	private String diseaseName;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "med_id")
+	private Med med;
+}
