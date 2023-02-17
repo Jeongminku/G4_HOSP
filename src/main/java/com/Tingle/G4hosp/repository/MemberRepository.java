a13e4cc2b804e726e7b34dbe3042a77c0824ecc5
Seosonak <nakoa346@gmail.com>
2023-02-17 14:30:53 +0900

    멤버서비스

--- src/main/java/com/Tingle/G4hosp/repository/MemberRepository.java
+++ src/main/java/com/Tingle/G4hosp/repository/MemberRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.Tingle.G4hosp.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
-	Member findById(String memberid); //아이디 중복 확인 
+	Member findByMemberId(String memberid); //아이디 중복 확인 
 }
--- src/main/java/com/Tingle/G4hosp/service/MemberService.java
+++ src/main/java/com/Tingle/G4hosp/service/MemberService.java
@@ -2,13 +2,49 @@ package com.Tingle.G4hosp.service;
 
 import javax.transaction.Transactional;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.Tingle.G4hosp.entity.Member;
+import com.Tingle.G4hosp.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService implements {
+public class MemberService implements UserDetailsService{
+	
+	 private final MemberRepository memberRepository;
 
+	@Override
+	public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+		
+		Member member = memberRepository.findByMemberId(memberId);
+		
+		if(member == null) {
+		 throw new UsernameNotFoundException(memberId);	
+		}
+			
+			
+		return User.builder()
+				   .username(member.getMemberid())
+				   .password(member.getPwd())
+				   .roles(member.getRole().toString())
+					.build();
+	
+	}
+
+	
+	public Member saveMember(Member member) {
+		return memberRepository.save(member);
+	}
+		
+		
+	
+	 
+	 
 }
