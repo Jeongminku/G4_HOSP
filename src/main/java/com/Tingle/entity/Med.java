f8e1bd5c90073c682f77401a59559c2e04b81b7a
Jeongminku <jmk00140@gmail.com>
2023-02-17 11:48:44 +0900

    minku

--- src/main/java/com/Tingle/entity/Med.java
+++ src/main/java/com/Tingle/entity/Med.java
@@ -1,6 +1,5 @@
 package com.Tingle.entity;
 
-<<<<<<< HEAD
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,45 +7,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@Table(name="med") 
-=======
-import javax.persistence.*;
-
 import lombok.*;
 
 @Entity
 @Table (name = "med")
->>>>>>> a7afb25556fee92da7cbc1006d1791634ead9c80
 @Getter
 @Setter
 @ToString
 public class Med {
-<<<<<<< HEAD
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "med_id")
 	private Long medId;
-	
+
+	@Column(name = "med_name")
 	private String medName;
 	
+	@Column(name = "med_info")
 	private String medInfo;
-	
-=======
-	@Id
-	@Column(name="med_id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	Long id;
-	
-	@Column(name="med_name")
-	String medName;
-	
-	@Column(name="med_info")
-	String medInfo;
->>>>>>> a7afb25556fee92da7cbc1006d1791634ead9c80
 }
