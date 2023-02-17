499bcd503c8a8358b2ec4a99e4ca7a0130431a97
Up-Min <korealee95@naver.com>
2023-02-17 14:19:15 +0900

    2.17 14:19 lee

---
+++ src/main/java/com/Tingle/G4hosp/repository/ArchiveImgRepository.java
@@ -0,0 +1,9 @@
+package com.Tingle.G4hosp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Tingle.G4hosp.entity.ArchiveImg;
+
+public interface ArchiveImgRepository extends JpaRepository<ArchiveImg, Long> {
+
+}
---
+++ src/main/java/com/Tingle/G4hosp/repository/HospitalizeRepository.java
@@ -0,0 +1,9 @@
+package com.Tingle.G4hosp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Tingle.G4hosp.entity.Hospitalize;
+
+public interface HospitalizeRepository extends JpaRepository<Hospitalize, Long>{
+
+}
