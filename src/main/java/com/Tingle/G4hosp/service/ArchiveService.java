27e99af44d5b1a14fe511d01bd77060a8545e119
Up-Min <korealee95@naver.com>
2023-02-20 18:23:47 +0900

    2.20 18:23 lee

--- src/main/java/com/Tingle/G4hosp/service/ArchiveService.java
+++ src/main/java/com/Tingle/G4hosp/service/ArchiveService.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.Tingle.G4hosp.dto.ArchiveFormDto;
 import com.Tingle.G4hosp.entity.Archive;
+import com.Tingle.G4hosp.entity.ArchiveImg;
 import com.Tingle.G4hosp.entity.Member;
 import com.Tingle.G4hosp.repository.MemberRepository;
 
@@ -20,15 +21,24 @@ import lombok.RequiredArgsConstructor;
 public class ArchiveService {
 	
 	private final MemberRepository memberRepository;
+	private final ArchiveImgService archiveImgService;
 	
 	
-	
-	public Long saveArchive(ArchiveFormDto archiveFormDto, List<MultipartFile> archiveImgFileList, Member mem) {
+	public Long saveArchive(ArchiveFormDto archiveFormDto, List<MultipartFile> archiveImgFileList, Member mem) throws Exception {
 		
-		Member member = memberRepository.findByLoginid(mem.getLoginid());
+		// SELECT MEMBER OBJECT (FIND DOCTOR BY NAME - USING DTO's DOCTOR NAME)
+		Member member = memberRepository.findbyNameindoctor(archiveFormDto.getDoctorname());
 		
+		// CREATE ARCHIVE BY MEMBER OBJECT
+		Archive archive = Archive.createArchive(member, archiveFormDto);
 		
-		return member.getId();
+		// SAVE EACH IMGFILE FROM IMGFILELIST
+		for(int i=0; i<archiveImgFileList.size(); i++) {
+			ArchiveImg archiveImg = new ArchiveImg();
+			archiveImg.setArchive(archive);
+			archiveImgService.saveArchiveImg(archiveImg, archiveImgFileList.get(i));
+		}
+		return archive.getId();
 	}
 	
 	
