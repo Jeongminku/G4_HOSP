1a66f09b5d4103e48168d8f0bb7a65950bf13707
Up-Min <korealee95@naver.com>
2023-02-20 10:53:02 +0900

    2.20 test lee

---
+++ src/main/java/com/Tingle/G4hosp/dto/ArchiveImgDto.java
@@ -0,0 +1,16 @@
+package com.Tingle.G4hosp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArchiveImgDto {
+	private Long id;
+	
+	private String imgname;
+	
+	private String imgurl;
+
+	private String imgori;
+}
