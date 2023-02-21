552ceb717e0b3aa266f66ebe1dcf96b7c2db7044
Inqui012 <xii0712@gmail.com>
2023-02-21 16:37:47 +0900

    예약관련 기능 거의 완성

--- src/main/java/com/Tingle/G4hosp/controller/ReservationController.java
+++ src/main/java/com/Tingle/G4hosp/controller/ReservationController.java
@@ -1,6 +1,9 @@
 package com.Tingle.G4hosp.controller;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Tingle.G4hosp.dto.ReservationDto;
+import com.Tingle.G4hosp.dto.ReservationViewDto;
+import com.Tingle.G4hosp.entity.Reservation;
+import com.Tingle.G4hosp.service.ReservationNotAvailableService;
 import com.Tingle.G4hosp.service.ReservationService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class ReservationController {
 	
 	private final ReservationService reservationService;
+	private final ReservationNotAvailableService reservationNotAvailableService;
 	
 	@GetMapping("/selectDoc")
 	public String selectDocPage (Model model) {
@@ -34,8 +42,50 @@ public class ReservationController {
 	}
 	
 	@PostMapping("/checkIn")
-	public String createReservation (ReservationDto reservationDto, Principal principal) {
-		System.err.println(reservationDto);
-		return "ReservationPage/SelectDate";
+	public String createReservation (Model model, ReservationDto reservationDto, Principal principal) {
+		try {
+			Reservation reservationed = reservationService.createReservation(reservationDto, principal.getName());
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM월 dd일 HH시");
+			model.addAttribute("ReservationedDate", reservationed.getReservationDate().format(formatter));
+		} catch (Exception e) {
+			model.addAttribute("Error", e.getMessage());
+		}
+		return "ReservationPage/ReservationComplete";
+	}
+	
+	@GetMapping("/listView")
+	public String reservationListView (Model model, Principal principal) {
+//		if(principal == null) {
+//			model.addAttribute("Error", "Need Login");
+//			return "member/memberLoginForm";
+//		}
+		try {
+			List<ReservationViewDto> viewList = reservationService.findAllReservationByDoctor(principal.getName());
+			model.addAttribute("ViewList", viewList);
+		} catch (Exception e) {
+			model.addAttribute("Error", e.getMessage());
+		}
+		return "ReservationPage/ViewReservation";
+	}
+	
+	@GetMapping("/setNotAvailDay")
+	public String setNotAvailableDay (Model model, Principal principal) {
+		return "ReservationPage/SetNotAvailableDay";
+	}
+	
+	@PostMapping("/setNotAvailDay")
+	public String setNotAvailableDayToDB (@RequestParam(name = "notAvailDate", required = false) List<String> notAvailDateList,
+										  @RequestParam("dayCheck") List<String> notAvailDayList,
+										  Principal principal, Model model) {
+		try {
+			reservationNotAvailableService.createReservationNotAvailable(principal.getName(), notAvailDateList, notAvailDayList);
+			List<String> notAvail = reservationNotAvailableService.findAllNotAvailByDoctor(principal.getName());
+			List<ReservationViewDto> viewList = reservationService.findAllReservationByDoctor(principal.getName());
+			model.addAttribute("NotAvail", notAvail);
+			model.addAttribute("ViewList", viewList);
+		} catch (Exception e) {
+			model.addAttribute("Error", e.getMessage());
+		}
+		return "ReservationPage/SetNotAvailableDay";
 	}
 }
--- src/test/java/com/Tingle/G4hosp/controller/ReservationControllerTest.java
+++ src/test/java/com/Tingle/G4hosp/controller/ReservationControllerTest.java
@@ -1,30 +1,31 @@
 package com.Tingle.G4hosp.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-
-import javax.persistence.EntityNotFoundException;
-import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.Tingle.G4hosp.constant.Role;
 import com.Tingle.G4hosp.dto.MedFormDto;
 import com.Tingle.G4hosp.dto.MemberFormDto;
+import com.Tingle.G4hosp.dto.ReservationDto;
+import com.Tingle.G4hosp.dto.ReservationViewDto;
 import com.Tingle.G4hosp.entity.Med;
 import com.Tingle.G4hosp.entity.Member;
 import com.Tingle.G4hosp.entity.MemberMed;
+import com.Tingle.G4hosp.entity.Reservation;
 import com.Tingle.G4hosp.repository.MedRepository;
 import com.Tingle.G4hosp.repository.MemberMedRepository;
 import com.Tingle.G4hosp.repository.MemberRepository;
+import com.Tingle.G4hosp.service.ReservationService;
 
 @SpringBootTest
-//@Transactional
+@Transactional
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ReservationControllerTest {
 
@@ -34,6 +35,8 @@ class ReservationControllerTest {
 	private MemberMedRepository memberMedRepository;
 	@Autowired
 	private MemberRepository memberRepository;
+	@Autowired
+	private ReservationService reservationService;
 	
 	public Med createMed(String name, String info) {
 		MedFormDto dto = new MedFormDto();
@@ -56,12 +59,26 @@ class ReservationControllerTest {
 		return memberRepository.save(dto.createMember());
 	}
 	
+	public Member createPat (String id, String name, String pw, String tel, String birth) {
+		MemberFormDto dto = new MemberFormDto();
+		dto.setBirth(birth);
+		dto.setLoginid(id);
+		dto.setName(name);
+		dto.setPwd(pw);
+		dto.setTel(tel);
+		dto.setRole(Role.CLIENT);
+		dto.setImgUrl("test");
+		dto.setImgOri("test");
+		dto.setImgName("test");
+		return memberRepository.save(dto.createMember());
+	}
+	
 	public MemberMed connectMedMember (Member doc, Med div) {
 		return memberMedRepository.save(MemberMed.createMemberMed(doc, div));
 	}
 	
 	
-	@Test
+//	@Test
 	@DisplayName("createTest")
 	void test() {
 		for(int i = 1; i < 6; i++) {
@@ -83,6 +100,35 @@ class ReservationControllerTest {
 				System.err.println(doctor.getName());
 			}
 			System.err.println("====================");			
+		}
+	}
+	
+//	@Test
+	@DisplayName("reservationTest")
+	void test2 () {
+		Member doctor = createDoc("doc", "doc1", "1234", "000", "000");
+		Member patient = createPat("pat", "pat1", "1234", "000", "000");
+		ReservationDto dto = new ReservationDto();
+		dto.setReservationDoctorId(doctor.getId());
+		dto.setReservationDate("2023-02-21T12:00:00");
+		Reservation reservation = reservationService.createReservation(dto, patient.getLoginid());
+		System.err.println(reservation);
+	}
+	
+	@Test
+	@DisplayName("callReservation")
+	void test3 () {
+		Member doctor = createDoc("doc", "doc1", "1234", "000", "000");
+		Member patient = createPat("pat", "pat1", "1234", "000", "000");
+		for(int i = 0; i < 10; i++) {
+			ReservationDto dto = new ReservationDto();
+			dto.setReservationDoctorId(doctor.getId());
+			dto.setReservationDate("2023-02-1" + i + "T12:00:00");
+			reservationService.createReservation(dto, patient.getLoginid());			
+		}
+		List<ReservationViewDto> view = reservationService.findAllReservationByDoctor(doctor.getLoginid());
+		for(ReservationViewDto dto : view) {
+			System.err.println(dto);
 		}
 	}
 
