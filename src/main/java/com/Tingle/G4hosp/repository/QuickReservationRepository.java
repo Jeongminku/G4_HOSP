4e7e256550ccaff0298c6962cb1deaf5c0b288c1
Up-Min <korealee95@naver.com>
2023-02-27 18:12:36 +0900

    2.27 18:12

---
+++ src/main/java/com/Tingle/G4hosp/repository/QuickReservationRepository.java
@@ -0,0 +1,9 @@
+package com.Tingle.G4hosp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Tingle.G4hosp.entity.QuickReservation;
+
+public interface QuickReservationRepository extends JpaRepository<QuickReservation, Long>{
+	
+}
