efc65202b807764850b097257065b63cbf00624b
Jeongminku <jmk00140@gmail.com>
2023-03-10 10:28:14 +0900

    mk

--- src/main/java/com/Tingle/G4hosp/G4hospApplication.java
+++ src/main/java/com/Tingle/G4hosp/G4hospApplication.java
@@ -2,9 +2,16 @@ package com.Tingle.G4hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class G4hospApplication {
+public class G4hospApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(G4hospApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(G4hospApplication.class, args);
