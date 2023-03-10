2e3284d5ea6cca16ca8b7504672dd8791ac0ffac
github harakangm <github diehfkszld@gmail.com>
2023-03-10 10:24:12 +0900

    up

--- src/main/java/com/Tingle/G4hosp/controller/BoardController.java
+++ src/main/java/com/Tingle/G4hosp/controller/BoardController.java
@@ -66,7 +66,7 @@ public class BoardController {
 		
 		if(req.getAttribute("isAdmin") != null) model.addAttribute("isAdmin", true);
 		
-		return "boardpage/BoardMain";
+		return "boardpage/boardMain";
 	}
 	
 	//글쓰기 페이지로 이동
@@ -103,6 +103,38 @@ public class BoardController {
 			Authentication authentication) {
 		    
 		    
+		
+		//쿠키를 통한 게시글 조회수 중복 카운트 방지
+		 Cookie oldCookie = null;
+		 Cookie[] cookies = request.getCookies();
+		 
+		    if (cookies != null) {
+		        for (Cookie cookie : cookies) {
+       	
+		            if (cookie.getName().equals("postView")) {		            	
+		                oldCookie = cookie;
+		            }
+		        }
+		    }
+
+		    if (oldCookie != null) {
+		    		
+		        if (!oldCookie.getValue().contains("[" + boardId.toString() + "]")) {
+		    
+		        	boardSerivce.updateview(boardId);
+		            oldCookie.setValue(oldCookie.getValue() + "_[" + boardId + "]");
+		            oldCookie.setPath("/");
+		            oldCookie.setMaxAge(60 * 60 * 24);
+		            response.addCookie(oldCookie);
+		        }
+		    } else {
+		 
+		    	boardSerivce.updateview(boardId);
+		        Cookie newCookie = new Cookie("postView","[" + boardId + "]");
+		        newCookie.setPath("/");
+		        newCookie.setMaxAge(60 * 60 * 24);
+		        response.addCookie(newCookie);
+		    }
 		    
 		    
 		    BoardFormDto boardFormDto = boardSerivce.getBoardDtl(boardId);
@@ -122,38 +154,7 @@ public class BoardController {
 		    	return MemberCheckMethod.redirectAfterAlert("비밀글입니다.",  "/board/main" , response);
 		    }
 		    
-		
-			//쿠키를 통한 게시글 조회수 중복 카운트 방지
-			 Cookie oldCookie = null;
-			 Cookie[] cookies = request.getCookies();
-			 
-			    if (cookies != null) {
-			        for (Cookie cookie : cookies) {
-	        	
-			            if (cookie.getName().equals("postView")) {		            	
-			                oldCookie = cookie;
-			            }
-			        }
-			    }
-
-			    if (oldCookie != null) {
-			    		
-			        if (!oldCookie.getValue().contains("[" + boardId.toString() + "]")) {
-			    
-			        	boardSerivce.updateview(boardId);
-			            oldCookie.setValue(oldCookie.getValue() + "_[" + boardId + "]");
-			            oldCookie.setPath("/");
-			            oldCookie.setMaxAge(60 * 60 * 24);
-			            response.addCookie(oldCookie);
-			        }
-			    } else {
-			 
-			    	boardSerivce.updateview(boardId);
-			        Cookie newCookie = new Cookie("postView","[" + boardId + "]");
-			        newCookie.setPath("/");
-			        newCookie.setMaxAge(60 * 60 * 24);
-			        response.addCookie(newCookie);
-			    }
+	
 		    
 		    
 			Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0 , 5);
